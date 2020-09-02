143eb9491b575a427d7501fd817204551f13a63c
philspence <philspence91@gmail.com>
2020-09-02 10:41:25 +0100

    added a few basic functions, will add more on an ad hoc basis

---
+++ cheminfo_scripts.py
@@ -0,0 +1,43 @@
+import rdkit
+from rdkit import Chem
+from rdkit.Chem import AllChem, Draw
+import os
+from os.path import join
+
+
+def sdfs_to_mols(sdfs):
+    mols = []
+    for i in sdfs:
+        suppl = Chem.SDMolSupplier(i)
+        num = 0
+        for mol in suppl:
+            mol.SetProp('_Name', f'{i}_mol_{num}')
+            mols.append(mol)
+            num += 1
+    return mols
+
+
+def sdfs_in_dir(dir_path):
+    return [join(dir_path, f) for f in os.listdir(dir_path) if f.endswith('.sdf')]
+
+
+def mols_to_2d(mols, out_file, per_row=4, img_size=(600, 600), legends=False):
+    if legends:
+        img = Draw.MolsToGridImage(mols, molsPerRow=per_row, subImgSize=img_size,
+                                   legends=[m.GetProp("_Name") for m in mols])
+    else:
+        img = Draw.MolsToGridImage(mols, molsPerRow=per_row, subImgSize=img_size)
+    img.save(out_file)
+    return
+
+
+def dir_sdfs_to_2d(dir_path, legends=False):
+    sdfs = sdfs_in_dir(dir_path)
+    mols = sdfs_to_mols(sdfs)
+    mols_to_2d(mols, join(dir_path, 'grid_image.png'), legends=legends)
+    return
+
+
+def smi_to_2d(smi):
+    mol = Chem.MolFromSmiles(smi)
+    Draw.ShowMol(mol)
